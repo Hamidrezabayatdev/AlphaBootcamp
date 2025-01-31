fd6244a56a2cb86c83e965ba44766342553e05a3
Hamidreza-Bayat <dev.hamidrezabayat@gmail.com>
2025-01-31 18:06:34 +0330

    after monthes

--- scripts/todo.js
+++ scripts/todo.js
@@ -11,16 +11,31 @@ let taskDown = document.querySelector(".newtask__tags__down");
 let taskMid = document.querySelector(".newtask__tags__mid");
 let taskUp = document.querySelector(".newtask__tags__up");
 let todos = document.querySelector(".todos");
+let todosDone = document.querySelector(".todo__done");
 // let taskDelete = document.querySelector(".delete__task");
 let taskEdit = document.querySelector(".edit__task");
 let todoTasks;
+let doneTasks;
 // localStorage.setItem("todoTasks", JSON.stringify([]));
 JSON.parse(localStorage.getItem("todoTasks")) ? (todoTasks = JSON.parse(localStorage.getItem("todoTasks"))) : (todoTasks = []);
+JSON.parse(localStorage.getItem("doneTasks")) ? (doneTasks = JSON.parse(localStorage.getItem("doneTasks"))) : (doneTasks = []);
 let task = {
     // id: 0,
     title: "",
     description: "",
     priority: ""
+}
+function numShow() {
+    let todayDescription = document.querySelector(".todo__description");
+    let doneDescription = document.querySelector(".done__description");
+    if (todoTasks.length == 0)
+        todayDescription.innerText = "کاری نمونده که انجام بدی؟";
+    else
+        todayDescription.innerText = "" + todoTasks.length + "تسک را باید انجام دهید";
+    if (doneTasks.length == 0)
+        doneDescription.innerText = "امروز کاری انجام ندادی!";
+    else
+        doneDescription.innerText = "" + doneTasks.length + " تسک انجام شده است";
 }
 function sortTodoTasks(todoTasks) {
     todoTasksSorted = []
@@ -52,6 +67,80 @@ function tagsBtnChange() {
 }
 function addPriority(priority) {
     task.priority = priority;
+}
+let age = 3;
+function doneCardCreate(task) {
+    const todoCard = document.createElement("div");
+    todoCard.classList.add("todo__card");
+    let prio = "";
+    switch (task.priority) {
+        case "up":
+            prio = "بالا";
+            break;
+        case "mid":
+            prio = "متوسط";
+            break;
+        case "down":
+            prio = "پایین";
+            break;
+        default:
+            break;
+    }
+    todoCard.innerHTML = `
+        <div class="todo__card__right">
+            <div class="todo__card__color todo__card__color__${task.priority}"></div>
+            <div class="todo__card__content">
+                <div class="todo__card__head">
+                    <input class="todo__checkbox" type="checkbox" name="state" id="card__check">
+                    <p style="text-decoration: line-through;" class="todo__card__title">${task.title}</p>
+                    <div class="todo__card__tag newtask__tags__${task.priority} tag">${prio}</div>
+                </div>
+                <p style="text-decoration: line-through;" class="todo__card__description">${task.description}</p>
+            </div>
+        </div>
+        <div class="todo__card__options">
+            <svg class="todo__card__options__icon" width="4" height="18" viewBox="0 0 4 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="2" cy="2" r="2" fill="#525252"/>
+                <circle cx="2" cy="9" r="2" fill="#525252"/>
+                <circle cx="2" cy="16" r="2" fill="#525252"/>
+            </svg>
+            <div class="todo__card__options__box">
+                <svg class="delete__task" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                    <path d="M4 7H20M5 7L6 19C6 19.5304 6.21071 20.0391 6.58579 20.4142C6.96086 20.7893 7.46957 21 8 21H16C16.5304 21 17.0391 20.7893 17.4142 20.4142C17.7893 20.0391 18 19.5304 18 19L19 7M9 7V4C9 3.73478 9.10536 3.48043 9.29289 3.29289C9.48043 3.10536 9.73478 3 10 3H14C14.2652 3 14.5196 3.10536 14.7071 3.29289C14.8946 3.48043 15 3.73478 15 4V7M10 12L14 16M14 12L10 16" stroke="#5C5F61" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+                <div class="newtask__tags__sep edit__sep"></div>
+                <svg class="edit__task" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                    <path d="M7 7H6C5.46957 7 4.96086 7.21071 4.58579 7.58579C4.21071 7.96086 4 8.46957 4 9V18C4 18.5304 4.21071 19.0391 4.58579 19.4142C4.96086 19.7893 5.46957 20 6 20H15C15.5304 20 16.0391 19.7893 16.4142 19.4142C16.7893 19.0391 17 18.5304 17 18V17" stroke="#5C5F61" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M16 4.99998L19 7.99998M20.385 6.58499C20.7788 6.19114 21.0001 5.65697 21.0001 5.09998C21.0001 4.543 20.7788 4.00883 20.385 3.61498C19.9912 3.22114 19.457 2.99988 18.9 2.99988C18.343 2.99988 17.8088 3.22114 17.415 3.61498L9 12V15H12L20.385 6.58499Z" stroke="#5C5F61" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+            </div>
+        </div>
+    `;
+    const todoOptions = todoCard.querySelector(".todo__card__options__icon");
+    const todoOptionsBox = todoCard.querySelector(".todo__card__options__box");
+    const todoEditInOptionBox = todoCard.querySelector(".edit__task");
+    const optionBoxSep = todoCard.querySelector(".edit__sep");
+    todoOptions.addEventListener("click", () => {
+        if (todoOptionsBox.style.display == "flex"){
+            todoOptionsBox.style.display = "none";
+            todoEditInOptionBox.style.display = "none";
+            optionBoxSep.style.display = "none";
+        }
+        else{
+            todoOptionsBox.style.display = "flex";
+            todoEditInOptionBox.style.display = "none";
+            optionBoxSep.style.display = "none";
+        }
+    });
+    let taskDelete = todoCard.querySelector(".delete__task");
+    taskDelete.addEventListener("click", () => {
+    let index = taskIndex(taskDelete, doneTasks);
+    doneTasks.splice(index, 1);
+    todosDone.innerHTML = ``;
+    doneShow();
+    // console.log(index);
+    });
+    return todoCard;
 }
 function todoCardCreate(task) {
     const todoCard = document.createElement("div");
@@ -170,21 +259,39 @@ function todoCardCreate(task) {
             todoEdit.style.display = "none";
             console.log(taskObj)
             todoTasks[index] = taskObj;
-            localStorage.setItem("todoTasks", JSON.stringify(todoTasks));
             todos.innerHTML = ``;
             todosShow();
         });
         // todos.appendChild(todoEdit);
+    });
+    let todoDone = todoCard.querySelector(".todo__checkbox");
+    todoDone.addEventListener("click", () => {
+        let index = taskIndex(taskDelete, todoTasks);
+        doneTasks.push(todoTasks[index]);
+        todoTasks.splice(index, 1);
+        todos.innerHTML = ``;
+        todosDone.innerHTML = ``;
+        todosShow();
+        doneShow();
     });
     return todoCard;
 }
 
 
 function todosShow() {
+    numShow()
     localStorage.setItem("todoTasks", JSON.stringify(todoTasks));
     // todos.innerHTML = ``;
     todoTasks.forEach(task => {
         todos.appendChild(todoCardCreate(task));
+    });
+}
+function doneShow() {
+    numShow()
+    localStorage.setItem("doneTasks", JSON.stringify(doneTasks));
+    // todos.innerHTML = ``;
+    doneTasks.forEach(task => {
+        todosDone.appendChild(doneCardCreate(task));
     });
 }
 function taskIndex(taskDelete, todoTasks) {
@@ -193,9 +300,10 @@ function taskIndex(taskDelete, todoTasks) {
     for (let i = 0; i < todoTasks.length; i++) {
         if (todoTasks[i].title == todoTitle)
             return i;
-    }    
+    }
 }
 document.addEventListener("DOMContentLoaded", () => {
+    doneShow()
     todosShow()
     if (todoTasks.length)
         todoEmpty.style.display = "none";
@@ -223,6 +331,7 @@ addTaskBtn.addEventListener("click", () => {
     taskDescription.value = "";
     task.priority = ""; // Reset priority
     todoTasks.push(newTaskObj);
+    numShow()
     localStorage.setItem("todoTasks", JSON.stringify(todoTasks));
     todos.appendChild(todoCardCreate(newTaskObj));
 });
